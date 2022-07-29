1c7533b00b88062f5b8e7ae12f1aed94d4f43a4d
lukas.stoeger@dehn.de <lukas.stoeger@dehn.de>
2022-07-29 13:27:31 +0200

    dev commented out

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { Server } from './server/index';
 import { rdm } from './rdm';
 import { evaluate } from './evaluation';
 import { initService } from './services/init';
-import { config as dotenv } from 'dotenv';
 import * as cron from 'node-cron';
 
 const server = new Server();
@@ -14,10 +13,10 @@ try {
     server.start(+config.app.port);
     initService.init();
 
-    // cron.schedule('* */4 * * 1-5', () => {
-    //     console.log('Data Acquisition and Playbook Generation runs every four hours');
-    //     rdm.run();
-    // });
+    cron.schedule('* */4 * * 1-5', () => {
+        console.log('Data Acquisition and Playbook Generation runs every four hours');
+        rdm.run();
+    });
 
     if (process.env.GENERATE_EVALUATION_OUTPUT === 'true') {
         evaluate.generateEvalOutput();
